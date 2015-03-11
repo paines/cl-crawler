0566cd9342a76cf314e0a70da2add2b4650327c6
Anes Lihovac <paines@Aness-Mac-mini.local>
2015-03-11 21:07:00 +0100

    added texture and key handling

--- crawler.lisp
+++ crawler.lisp
@@ -1,29 +1,64 @@
 (eval-when (:compile-toplevel :load-toplevel :execute)
   (declaim (optimize (speed 0) (compilation-speed 0) (safety 0) (debug 0)))
   (asdf:load-system :sdl2kit)
-  (asdf:load-system :cl-jpeg)
   (asdf:load-system :png-read)
   (asdf:load-system :static-vectors)
-  (asdf:load-system :cl-opengl)
+;  (asdf:load-system :cl-opengl)
   )
+
+(defvar *base-pathname* (or *load-truename* *compile-file-truename*))
+
+(defun asset-path (file)
+  (format t "loading file ~A~%"(merge-pathnames file *base-pathname*))
+  (merge-pathnames file *base-pathname*))
+
 
 (defparameter *state* 0)
 
-(defparameter width 240)
-(defparameter height 160)
+(defparameter width 160)
+(defparameter height 120)
 
 (defparameter half-height (/ height 2))
 (defparameter half-width (/ width 2))
 
-(defparameter window-width (* width 5))
-(defparameter window-height (* height 5))
+(defparameter window-width (* width 4))
+(defparameter window-height (* height 4))
 
 (defparameter *num-frames* 0)
 (defparameter *last-ticks* 0)
 (defparameter *diff-ticks* 0)
-(defparameter *image* 0)
 
+(defparameter *xpos* 0)
+(defparameter *ypos* 0)
+
+(defparameter *step-x* 3)
+(defparameter *step-y* 3)
+(defparameter *light* 6)
+(defparameter *image* 0)
+(defparameter *phi* 0)
 (defstruct (image :conc-name) data width height depth pitch)
+
+(defparameter *pi/180* 0.017453292519943295d0)
+
+(defparameter yd-values 0)
+
+(defparameter *xrel-old* 0)
+(defparameter *xrel-new* 0)
+
+(defparameter *yrel-old* 0)
+(defparameter *yrel-new* 0)
+
+(setf yd-values (make-array (list height) :element-type 'float))
+(dotimes (y height)
+  (setf (aref yd-values y) (/ (- (+ y 0.5) half-height) height)))
+
+(defparameter xd-values 0)
+
+(setf xd-values (make-array (list width) :element-type 'float))
+(dotimes (x width)
+  (setf (aref xd-values x) (/ (- x half-width) height)))
+
+
 
 (declaim (inline getOffset))
 (defun getOffset (x y w)
@@ -31,11 +66,11 @@
   (the fixnum (+ (* y w) x)))
 
 
-(defun gl-ortho-setup (&key (width 500) (height 500))
-  "Set up 1:1 pixel ortho matrix"
-  (gl:viewport 0 0 width height)
-  (gl:matrix-mode :projection)
-  (gl:ortho 0 width height 0 -1 1))
+;; (defun gl-ortho-setup (&key (width 500) (height 500))
+;;   "Set up 1:1 pixel ortho matrix"
+;;   (gl:viewport 0 0 width height)
+;;   (gl:matrix-mode :projection)
+;;   (gl:ortho 0 width height 0 -1 1))
 
 ;;stolen form https://github.com/froggey/Mezzano/blob/master/gui/desktop.lisp - thanks mate!
 (defun load-png (path)
@@ -67,9 +102,17 @@
       (make-image :data array :width width :height height :depth size :pitch (* width size)))))
 
 (defun render (renderer window-width window-height width height image state)
+
+
+  ;; (if (= *phi* 360)
+  ;;     (setf *phi* 0))
+  
+  ;; (setf *phi* (+ *phi* 1))
+	
   (if (>= *diff-ticks* 1000) 
       (progn
-	(format t "frames = ~D ~%" *num-frames*)    
+
+	(format t "frames = ~D phi = ~D ~%" *num-frames* *phi*)    
 	(setf *num-frames* 0)
 	(setf *diff-ticks* 0)
 	(finish-output)))
@@ -82,31 +125,47 @@
 	 (z 0)
 	 (xx 0.0)
 	 (yy 0.0)
-	 (brightness .5)
+	 (x1 0)
+	 (y1 0)
+	 (a 0)
+	 (brightness 1)
 	 (ticks *last-ticks*)
-	 (eye (+ (* (sin (/ ticks 666)) 2) (* (cos (/ ticks 666)) 2)))
-	 (size 35)
+;	 (eye (sin (* ticks 100)))
+	 (eye -5)
+	 (size 16)
 	 (src-rect (sdl2:make-rect 0 0 width height))
 	 (dest-rect (sdl2:make-rect 0 0 window-width window-height))
 	 (pixels (static-vectors:make-static-vector (* width height) :element-type '(unsigned-byte 32) :initial-element 0))
-	 (zbuffer (static-vectors:make-static-vector (* width height) :element-type '(unsigned-byte 32) :initial-element 0)))
+	 (zbuffer (static-vectors:make-static-vector (* width height) :element-type '(unsigned-byte 32) :initial-element 0))
+	 (offset 0))
 
     (dotimes (y height)
 
-       (setf yd (/ (- (+ y 0.5) half-height) height))
+      ;;(setf yd (/ (- (+ y 0.5) half-height) height))
+      (setf yd (aref yd-values y))
       
-       (setf z (/ (+ size eye) yd))
-       (if (< yd 0)
+      (setf z (/ (+ size eye) yd))
+      (if (< yd 0)
        	  (setf z   (/ (- size eye) (- yd))))
       
       (dotimes (x width)
-       	(setf xd (* (/ (- x half-width) height) z))
-	
- 	(setf xx (logand (truncate xd) (- (width image) 1)))
- 	(setf yy (logand (truncate z) (- (height image) 1)))
 
-       	(setf (aref zbuffer (getOffset x y width)) (truncate (* z brightness)))
-       	(setf (aref pixels (getOffset x y width)) (aref (data image) (getOffset xx yy (width image))))))
+	;;(setf xd (+ (* (/ (- x half-width) height) z) (/ ticks 10)))
+	;;division by 10 from http://electronics.stackexchange.com/questions/12618/fastest-way-to-get-integer-mod-10-and-integer-divide-10
+;	(setf xd (+ (* (aref xd-values x) z) (ash (* ticks 205) -11)))
+	(setf xd (+ (* (aref xd-values x) z)))
+
+	(setf a (/ (* *phi* pi) 180))
+	
+	(setf x1 (- (* xd (cos a)) (* z (sin a)))) 
+	(setf y1 (+ (* xd (sin a)) (* z (cos a)))) 
+	
+ 	(setf xx (logand (truncate (+ x1 *xpos*)) (- (width image) 1)))
+ 	(setf yy (logand (truncate (+ y1 *ypos*)) (- (height image) 1)))
+
+	(setf offset (getOffset x y width))
+       	(setf (aref zbuffer offset) (truncate (* z brightness)))
+       	(setf (aref pixels offset) (aref (data image) (getOffset xx yy (width image))))))
 
     
     (post-process pixels zbuffer width height)
@@ -141,15 +200,15 @@
       (setf g (logand #xff (ash col -8)))
       (setf b (logand #xff col))
       
-      (setf brightness (the fixnum (- 255 (aref zbuffer i))))
+      (setf brightness (round (/ 20000 (* (aref zbuffer i) *light*))))
       ;(setf brightness (- 255 (aref zbuffer i)))
       
       (if (< brightness 0)
 	  (setf brightness 0))
 
       (setf r (the fixnum (ash (* r brightness) -8)))
-      (setf g (truncate (ash (* g brightness) -8)))
-      (setf b (truncate (ash (* b brightness) -8)))
+      (setf g (the fixnum (ash (* g brightness) -8)))
+      (setf b (the fixnum  (ash (* b brightness) -8)))
 
       (setf res (logior
 		 (ash a 24)
@@ -159,17 +218,63 @@
       
       (setf (aref pixels i) res))))
 
+(defun handle-mouse (x y xrel yrel state)
+  
+  (setf *xrel-new* (- xrel *xrel-old*))
+  (setf *xrel-old* xrel)
+
+  (setf *phi* (- x)))
+  
+  
+  ;;  (format t "Mouse motion abs(rel): ~a (~a), ~a (~a)~%Mouse state: ~a~%"
+  ;;	  x xrel y yrel state)
+
+
+(defun handle-inputs (keysym)
+  (when (sdl2:scancode= (sdl2:scancode-value keysym) :scancode-escape)
+    (sdl2:push-event :quit))
+  
+  (when (sdl2:scancode= (sdl2:scancode-value keysym) :scancode-p)
+    (setf *state* 1))
+  
+  (when (sdl2:scancode= (sdl2:scancode-value keysym) :scancode-o)
+    (setf *state* 0))
+
+  (when (sdl2:scancode= (sdl2:scancode-value keysym) :scancode-a)
+    (setf *xpos* (- *xpos* *step-x*)))
+  
+  (when (sdl2:scancode= (sdl2:scancode-value keysym) :scancode-d)    
+    (setf *xpos* (+ *xpos* *step-x*)))
+
+  (when (sdl2:scancode= (sdl2:scancode-value keysym) :scancode-w)
+    (setf *ypos* (+ *ypos* *step-y*)))
+
+  (when (sdl2:scancode= (sdl2:scancode-value keysym) :scancode-s)
+    (setf *ypos* (- *ypos* *step-y*)))
+
+  
+  (when (sdl2:scancode= (sdl2:scancode-value keysym) :scancode-k)    
+    (setf *light* (- *light* 1))
+    (if (< *light* 0)
+	(setf *light* 1)))
+  
+  
+  (when (sdl2:scancode= (sdl2:scancode-value keysym) :scancode-l)    
+    (setf *light* (+ *light* 1))
+    (if (< *light* 0)
+	(setf *light* 1))))
+
 
 (defun basic-test ()
-  (setf *image* (load-png #p"~/Dropbox/mario.png"))
+  (setf *image* (load-png (asset-path #p"floor-tile.png")))
   (sdl2:with-init (:everything)
     (multiple-value-bind (window renderer)
-;;	(sdl2:create-window-and-renderer window-width window-height '(:shown))
-        (sdl2:create-window-and-renderer window-width window-height '(:shown :opengl))
-      (sdl2:with-gl-context (gl window)
-        (sdl2:gl-make-current window gl)
-        (gl:enable :texture-2d)
-        (gl-ortho-setup :width window-width :height window-height)
+	(sdl2:create-window-and-renderer window-width window-height '(:shown))
+      ;;   (sdl2:create-window-and-renderer window-width window-height '(:shown :opengl))
+      ;; (sdl2:with-gl-context (gl window)
+      ;;   (sdl2:gl-make-current window gl)
+      ;;   (gl:enable :texture-2d)
+      ;;   (gl-ortho-setup :width window-width :height window-height)
 
 ;	(gl-set-attr 'attr value)
       ;; main loop
@@ -178,34 +283,26 @@
       (sdl2:with-event-loop (:method :poll)
 	(:keydown
 	 (:keysym keysym)
-	 (let ((scancode (sdl2:scancode-value keysym))
-	       (sym (sdl2:sym-value keysym))
-	       (mod-value (sdl2:mod-value keysym)))
-	   (cond
-	     ((sdl2:scancode= scancode :scancode-w) (format t "~a~%" "WALK"))
-	     ((sdl2:scancode= scancode :scancode-s) (sdl2:show-cursor))
-	     ((sdl2:scancode= scancode :scancode-h) (sdl2:hide-cursor)))
-	   (format t "Key sym: ~a, code: ~a, mod: ~a~%"
-		   sym
-		   scancode
-		   mod-value)))
+	 (handle-inputs keysym))
+	;;  (let ((scancode (sdl2:scancode-value keysym))
+	;;        (sym (sdl2:sym-value keysym))
+	;;        (mod-value (sdl2:mod-value keysym)))
+	;;    (cond
+	;;      ((sdl2:scancode= scancode :scancode-w) (format t "~a~%" "WALK"))
+	;;      ((sdl2:scancode= scancode :scancode-s) (sdl2:show-cursor))
+	;;      ((sdl2:scancode= scancode :scancode-h) (sdl2:hide-cursor)))
+	;;    (format t "Key sym: ~a, code: ~a, mod: ~a~%"
+	;; 	   sym
+	;; 	   scancode
+	;; 	   mod-value)))
 	
-	(:keyup
-	 (:keysym keysym)
-	 (when (sdl2:scancode= (sdl2:scancode-value keysym) :scancode-escape)
-	   (sdl2:push-event :quit))
-
-	 (when (sdl2:scancode= (sdl2:scancode-value keysym) :scancode-p)
-	   (setf *state* 1))
-
-	 (when (sdl2:scancode= (sdl2:scancode-value keysym) :scancode-o)
-	   (setf *state* 0)))
-	 
+	;; (:keyup
+	;;  (:keysym keysym))	 
 	
 	(:mousemotion
 	 (:x x :y y :xrel xrel :yrel yrel :state state)
-	 (format t "Mouse motion abs(rel): ~a (~a), ~a (~a)~%Mouse state: ~a~%"
-		 x xrel y yrel state))
+	 (handle-mouse x y xrel yrel state))
+	
 	(:idle
 	 ()
 	 (render renderer window-width window-height width height *image* *state*))
@@ -213,8 +310,9 @@
 	(:quit () t))
       (sdl2:destroy-renderer renderer)
       (sdl2:destroy-window window))))
-)
+;)
 
-#-clozure
+#+sbcl
 (sdl2:make-this-thread-main (lambda () (basic-test)))
+#+clozure
 (basic-test)
